0cf7b39a5f568e35677f9cda5c661fe00262be9c
tahmidawal <98552052+tahmidawal@users.noreply.github.com>
2024-07-18 23:04:33 -0400

    Add files via upload

---
+++ salesagent2.py
@@ -0,0 +1,202 @@
+import asyncio
+from dotenv import load_dotenv
+import requests
+import time
+import os
+import tempfile
+from pydub import AudioSegment
+from pydub.playback import play
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+
+from deepgram import (
+    DeepgramClient,
+    DeepgramClientOptions,
+    LiveTranscriptionEvents,
+    LiveOptions,
+    Microphone,
+)
+
+load_dotenv()
+
+class LanguageModelProcessor:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768", groq_api_key=os.getenv("GROQ_API_KEY"))
+        # self.llm = ChatOpenAI(temperature=0, model_name="gpt-4-0125-preview", openai_api_key=os.getenv("OPENAI_API_KEY"))
+        # self.llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-0125", openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+        self.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+        # Load the system prompt from a file
+        with open('system_prompt.txt', 'r') as file:
+            system_prompt = file.read().strip()
+        
+        self.prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            HumanMessagePromptTemplate.from_template("{text}")
+        ])
+
+        self.conversation = LLMChain(
+            llm=self.llm,
+            prompt=self.prompt,
+            memory=self.memory
+        )
+
+    def process(self, text):
+        self.memory.chat_memory.add_user_message(text)  # Add user message to memory
+
+        start_time = time.time()
+
+        # Go get the response from the LLM
+        response = self.conversation.invoke({"text": text})
+        end_time = time.time()
+
+        self.memory.chat_memory.add_ai_message(response['text'])  # Add AI response to memory
+
+        elapsed_time = int((end_time - start_time) * 1000)
+        print(f"LLM ({elapsed_time}ms): {response['text']}")
+        return response['text']
+
+class TextToSpeech:
+    # Set your Deepgram API Key and desired voice model
+    DG_API_KEY = os.getenv("DEEPGRAM_API_KEY")
+    MODEL_NAME = "aura-helios-en"  # Example model name, change as needed
+
+    def __init__(self):
+        # Set the path to the ffmpeg binary
+        AudioSegment.converter = "/usr/local/bin/ffmpeg"  # Change this to your path
+
+    def speak(self, text):
+        DEEPGRAM_URL = f"https://api.deepgram.com/v1/speak?model={self.MODEL_NAME}&performance=some&encoding=linear16&sample_rate=24000"
+        headers = {
+            "Authorization": f"Token {self.DG_API_KEY}",
+            "Content-Type": "application/json"
+        }
+        payload = {
+            "text": text
+        }
+
+        start_time = time.time()
+        response = requests.post(DEEPGRAM_URL, headers=headers, json=payload, stream=True)
+
+        if response.status_code == 200:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        temp_audio_file.write(chunk)
+                temp_audio_path = temp_audio_file.name
+
+            try:
+                audio = AudioSegment.from_wav(temp_audio_path)
+                play(audio)
+            finally:
+                os.remove(temp_audio_path)
+
+            end_time = time.time()
+            elapsed_time = int((end_time - start_time) * 1000)
+            print(f"TTS ({elapsed_time}ms): Audio played successfully.")
+        else:
+            print(f"Error: Unable to generate speech. Status code: {response.status_code}")
+
+class TranscriptCollector:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.transcript_parts = []
+
+    def add_part(self, part):
+        self.transcript_parts.append(part)
+
+    def get_full_transcript(self):
+        return ' '.join(self.transcript_parts)
+
+transcript_collector = TranscriptCollector()
+
+async def get_transcript(callback):
+    transcription_complete = asyncio.Event()  # Event to signal transcription completion
+
+    try:
+        config = DeepgramClientOptions(options={"keepalive": "true"})
+        deepgram: DeepgramClient = DeepgramClient(os.getenv("DEEPGRAM_API_KEY"), config)
+
+        dg_connection = deepgram.listen.asynclive.v("1")
+        print("Listening...")
+
+        async def on_message(self, result, **kwargs):
+            sentence = result.channel.alternatives[0].transcript
+            
+            if not result.speech_final:
+                transcript_collector.add_part(sentence)
+            else:
+                transcript_collector.add_part(sentence)
+                full_sentence = transcript_collector.get_full_transcript()
+                if len(full_sentence.strip()) > 0:
+                    full_sentence = full_sentence.strip()
+                    print(f"Human: {full_sentence}")
+                    callback(full_sentence)
+                    transcript_collector.reset()
+                    transcription_complete.set()
+
+        dg_connection.on(LiveTranscriptionEvents.Transcript, on_message)
+
+        options = LiveOptions(
+            model="nova-2",
+            punctuate=True,
+            language="en-US",
+            encoding="linear16",
+            channels=1,
+            sample_rate=16000,
+            endpointing=300,
+            smart_format=True,
+        )
+
+        await dg_connection.start(options)
+
+        microphone = Microphone(dg_connection.send)
+        microphone.start()
+
+        await transcription_complete.wait()
+
+        microphone.finish()
+        await dg_connection.finish()
+
+    except Exception as e:
+        print(f"Could not open socket: {e}")
+
+class ConversationManager:
+    def __init__(self):
+        self.transcription_response = ""
+        self.llm = LanguageModelProcessor()
+
+    async def main(self):
+        def handle_full_sentence(full_sentence):
+            self.transcription_response = full_sentence
+
+        while True:
+            await get_transcript(handle_full_sentence)
+            
+            if "goodbye" in self.transcription_response.lower():
+                break
+            
+            llm_response = self.llm.process(self.transcription_response)
+
+            tts = TextToSpeech()
+            tts.speak(llm_response)
+
+            self.transcription_response = ""
+
+if __name__ == "__main__":
+    manager = ConversationManager()
+    asyncio.run(manager.main())
